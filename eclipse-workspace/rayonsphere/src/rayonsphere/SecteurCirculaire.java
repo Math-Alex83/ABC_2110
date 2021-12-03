661d03333c637ffc3e1c8e3dc5a62b7d3bea7fa5
Math-Alex83 <92789487+pacificateur83@users.noreply.github.com>
2021-12-03 11:09:16 +0100

    début d'exo

--- eclipse-workspace/rayonsphere/src/rayonsphere/SecteurCirculaire.java
+++ eclipse-workspace/rayonsphere/src/rayonsphere/SecteurCirculaire.java
@@ -6,8 +6,14 @@ public class SecteurCirculaire {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//
 		
-		Scanner sc = 
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
