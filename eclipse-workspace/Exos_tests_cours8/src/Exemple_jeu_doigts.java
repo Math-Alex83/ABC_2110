634f91f1797d981928ede62269d7a70bba537481
Math-Alex83 <92789487+Math-Alex83@users.noreply.github.com>
2022-03-06 12:48:40 +0100

    exo vacances

--- eclipse-workspace/Exos_tests_cours8/src/Exemple_jeu_doigts.java
+++ eclipse-workspace/Exos_tests_cours8/src/Exemple_jeu_doigts.java
@@ -4,11 +4,12 @@ public class Exemple_jeu_doigts {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// jeux test avec les doigts
+		// jeux test avec les doigts
 		// Déclaration des variables
 		int nbrA;
 		int nbrB;
 		int reste;
+<<<<<<< Updated upstream
 		String reponse;
 		
 		// import du scanner
@@ -16,18 +17,31 @@ public class Exemple_jeu_doigts {
 		// Debut de la partie
 		do
 		{
+=======
+	
+		// import du scanner
+				Scanner sc = new Scanner(System.in);
+		// Début de la partie
+
+>>>>>>> Stashed changes
 		// écrire "Les joueurs A et B vont choisir un chiffre entre 0 et 5, si la sommes est paire: Joueur A Gagne ; Si impaire: Joueur B Gagne."
 		System.out.println("Les joueurs A et B choisiront un chiffre entre 0 et 5. Si, le résultat est paire, Joueur A Gagne, sinon jouer B ");
+		
+		
+<<<<<<< Updated upstream
+=======
 		//écrire" joueur A commence"
-		System.out.println("Commençons! Joueur A, entrez un chiffre entre 0 et 5 s'il vous plait.");
-		
-		
+		System.out.println("Commençons! Joueur A, entrez un chiffre entre 0 et 5 s'il vous plait."); 
+>>>>>>> Stashed changes
 		//Lire choix du joueur A
 		nbrA = sc.nextInt();
+		
+		
 		// écrire "A vous joueur B, entrez un chiffre entre 0 et 5 s'il vous plait."
 		System.out.println("A vous joueur B, entrez un chiffre entre 0 et 5 s'il vous plait.");
 		// Lire choix du joueur B
 		nbrB = sc.nextInt();
+<<<<<<< Updated upstream
 		
 		// Traitement
 		reste = ((nbrA + nbrB) % 2);
@@ -46,6 +60,12 @@ public class Exemple_jeu_doigts {
 		}
 		while (!reponse.equals("non"));
 	sc.close();	
+=======
+
+
+
+		sc.close();	
+>>>>>>> Stashed changes
 	}
 
 }
